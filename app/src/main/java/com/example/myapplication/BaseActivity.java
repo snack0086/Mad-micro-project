2f31f9aa394542ac2b3a329a70f9278f61320f74
Shifa Patel <shifa.pateldo@gmail.com>
2026-02-27 23:10:15 +0530

    resolved conflicts

---
+++ app/src/main/java/com/example/myapplication/BaseActivity.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected DrawerLayout drawerLayout;
+    protected NavigationView navigationView;
+    protected Toolbar toolbar;
+    protected String userRole;
+
+    protected void setupDrawer(int checkedItemId) {
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setCheckedItem(checkedItemId);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_dashboard) {
+
+                if ("teacher".equals(userRole)) {
+                    startActivity(new Intent(this, Teacher_dashboard.class));
+                } else if ("student".equals(userRole)) {
+                    startActivity(new Intent(this, StudentDashboardActivity.class));
+                }
+
+            }
+
+//            else if (id == R.id.nav_announcements) {
+//                startActivity(new Intent(this, announcementActivity.class));
+//            }
+
+            else if (id == R.id.nav_upload_material) {
+                startActivity(new Intent(this, LearningMaterialActivity.class));
+            }
+
+            else if (id == R.id.nav_manage_assignments) {
+                startActivity(new Intent(this, TeacherAssignmentActivity.class));
+            }
+
+//            else if (id == R.id.nav_profile) {
+//                startActivity(new Intent(this, ProfileActivity.class));
+//            }
+
+            else if (id == R.id.nav_logout) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, Login.class));
+                finish();
+            }
+
+            drawerLayout.closeDrawers();
+            return true;
+        });
+    }
+}
